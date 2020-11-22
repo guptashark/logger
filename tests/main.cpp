f090e29f65b6f67475bea9835ce3c04f04ff6a58
Aishwary Gupta <guptashark@protonmail.com>
2020-11-21 20:06:29 -0500

    Create folder for expected outputs from tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,7 +7,7 @@ int main(void) {
 
 	std::cout << "Logger Tests" << std::endl;
 
-	logger lg ( "tests/logger_test_01_actual.txt");
+	logger lg ( "tests/actual/logger_test_01_actual.txt");
 
 	lg.println("Logger initialized");
 	lg.trace_fn_begin("main");
